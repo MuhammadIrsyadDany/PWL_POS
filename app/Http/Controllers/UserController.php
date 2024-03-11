4a25b6fdbc1b1f5718629dbf03f4a40f1b4de86b
MuhammadIrsyadDany <irsyadnny09@gmail.com>
2024-03-11 20:38:09 +0700

    2.5 - Attribute Changes

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,16 +10,41 @@ class UserController extends Controller
 {
     public function index()
     {
-        $user = UserModel::firstOrNew(
+        $user = UserModel::create(
             [
-                'username' => 'manager33',
-                'nama' => 'Manager Tiga Tiga',
+                'username' => 'manager13',
+                'nama' => 'Manager13',
                 'password' => Hash::make('12345'),
                 'level_id' => 2,
             ]
         );
+
+        $user->username = 'manager14';
         $user->save();
-        return view('user', ['data' => $user]);
+
+        $user->wasChanged(); //true
+        $user->wasChanged('username'); //true
+        $user->wasChanged(['username', 'level_id']); //true
+        $user->wasChanged('nama'); //false
+
+        dd($user->wasChanged(['nama', 'username'])); // true
+
+        // $user->isDirty(); //true
+        // $user->isDirty('username'); //true
+        // $user->isDirty('nama'); //false
+        // $user->isDirty('nama', 'username'); //true
+
+        // $user->isClean(); //false
+        // $user->isClean('username'); //false
+        // $user->isClean('nama'); //true
+        // $user->isClean('nama', 'username'); //false
+
+
+
+        // $user->isDirty(); //false
+        // $user->isClean(); //true
+
+        // dd($user->isDirty());
 
         // coba akses model UserModel
         // $user = UserModel::all(); // ambil semua data dari tabel m_user
