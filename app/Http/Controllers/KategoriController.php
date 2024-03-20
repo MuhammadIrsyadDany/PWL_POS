1dcb605d90665ce046dec08606c62bfae911408e
MuhammadIrsyadDany <irsyadnny09@gmail.com>
2024-03-20 13:04:40 +0700

    Jobsheet 5 - Praktikum 1 & 2

--- app/Http/Controllers/KategoriController.php
+++ app/Http/Controllers/KategoriController.php
@@ -2,29 +2,13 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Support\Facades\DB;
-
+use App\DataTables\KategoriDataTable;
 use Illuminate\Http\Request;
 
 class KategoriController extends Controller
 {
-    public function index()
+    public function index(KategoriDataTable $dataTable)
     {
-        // $data = [
-        //     'kategori_code' => 'SNK',
-        //     'kategori_nama' => 'Snack/Makanan Ringan',
-        //     'created_at' => now()
-        // ];
-        // DB::table('m_kategori')->insert($data);
-        // return 'Insert data baru berhasil';
-
-        // $row = DB::table('m_kategori')->where('kategori_code', 'SNK')->update(['kategori_nama' => 'Cemilan']);
-        // return 'Update data berhasil. Jumlah data yang di update : ' . $row . ' baris';
-
-        // $row = DB::table('m_kategori')->where('kategori_code', 'SNK')->delete();
-        // return 'Delete data berhasil. Jumlah data yang di hapus : ' . $row . ' baris';
-
-        $data = DB::select('select * from m_kategori');
-        return view('kategori', ['data' => $data]);
+        return $dataTable->render('kategori.index');
     }
 }
